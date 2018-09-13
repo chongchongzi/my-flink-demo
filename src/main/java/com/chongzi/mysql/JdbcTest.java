f7623128a529a87f256d89f5cfbfc3104b232e39
chongchongzi <389401879@qq.com>
2018-09-13 18:09:04 +0800

    新增mysql demo

---
+++ src/main/java/com/chongzi/mysql/JdbcTest.java
@@ -0,0 +1,50 @@
+package com.chongzi.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * 本类主要用于检测jdbc连接是否成功
+ */
+public class JdbcTest {
+    public static void main(String[] args) throws Exception {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://127.0.0.1:3306/flinktest";
+        String username = "root";
+        String password = "123456";
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            //1.加载驱动
+            Class.forName(driver);
+            //2.创建连接
+            connection = DriverManager.getConnection(url, username, password);
+            //3.获得执行语句
+            statement = connection.createStatement();
+            //4.执行查询，获得结果集
+            ResultSet resultSet = statement.executeQuery(
+            "select stuid,stuname,stuaddr,stusex from Student");
+            //5.处理结果集
+            while (resultSet.next()) {
+                Student student = new Student(
+                resultSet.getInt("stuid"),
+                resultSet.getString("stuname").trim(),
+                resultSet.getString("stuaddr").trim(),
+                resultSet.getString("stusex").trim());
+                System.out.println(student);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //6.关闭连接，释放资源
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/chongzi/mysql/StudentSinkToMysqlTest.java
@@ -0,0 +1,26 @@
+package com.chongzi.mysql;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * sink测试程序
+ */
+public class StudentSinkToMysqlTest {
+    public static void main(String[] args) throws Exception {
+        //1.创建流执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //2.准备数据
+        DataStream<Student> students = env.fromElements(
+                new Student(11, "zhangsan", "beijing biejing", "female"),
+                new Student(12, "lisi", "tainjing tianjin", "male ")
+        );
+
+        //3.将数据写入到自定义的sink中（这里是mysql）
+        students.addSink(new StudentSinkToMysql());
+
+        //4.触发流执行
+        env.execute();
+    }
+}
